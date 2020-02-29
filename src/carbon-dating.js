eadb2aaa66a6c01c2050cbf55f69497a4425ad9c
Ekaterina Kirilenko <katerina_sanway@mail.ru>
2020-03-01 00:51:24 +0300

    feat: completed task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,19 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(finalActivity) {
+  let num = parseFloat(finalActivity);
+  if (
+    finalActivity === "undefined" ||
+    typeof finalActivity !== "string" ||
+    isNaN(num) ||
+    num <= 0 || num > 15
+  ) {
+    return false;
+  } else {
+    const s = 0.693;
+    let K = s / HALF_LIFE_PERIOD;
+    let t = Math.log(MODERN_ACTIVITY / num) / K;
+    return Math.ceil(t);
+  }
 };
