be5ad6ec05480e37e45675b5f6dfd8371e4aaf24
Ekaterina Kirilenko <katerina_sanway@mail.ru>
2020-03-01 23:26:36 +0300

    feat: completed task What season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,31 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (date === undefined) {
+      return "Unable to determine the time of year!";
+    } else if (isNaN(date)) {
+        throw new Error("Хитрый момент!");
+    } else {
+      let monthNumber = date.getMonth();
+      let monthString = "";
+
+      switch (monthNumber) {
+        case 0:
+        case 1:
+        case 11:
+          monthString = "winter";
+          break;
+        case 2:
+        case 3:
+        case 4:
+          monthString = "spring";
+          break;
+        case 5:
+        case 6:
+        case 7:
+          monthString = "summer";
+          break;
+        default:
+          monthString = "autumn";
+      }
+      return monthString;
+    } 
 };
