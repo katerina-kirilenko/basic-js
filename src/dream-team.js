b4b4d4aa80f5870efa516fd733d9433b4561510f
Ekaterina Kirilenko <katerina_sanway@mail.ru>
2020-03-01 20:21:48 +0300

    feat: completed task Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    let nameTeam = "";
+    let team = [];
+
+    if (Array.isArray(members)) {
+      for (let item of members) {
+        if (typeof item == "string") {
+          let str = item.trim().toUpperCase();
+          team.push(str[0]);
+        }
+      }
+      nameTeam = team.sort().join("");
+    }
+
+    return nameTeam;
 };
